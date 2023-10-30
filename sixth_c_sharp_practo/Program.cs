dfb68f76d38faa2689196360d7b6918d330ce936
EtoNeAnanas95 <ananasovii.yashik@gmail.com>
2023-10-30 16:58:49 +0300

    update

--- sixth_c_sharp_practo/Program.cs
+++ sixth_c_sharp_practo/Program.cs
@@ -11,102 +11,60 @@ namespace sixth_c_sharp_practo
 {
     internal class Program
     {
-        static void move_cursor(int y, int x, ConsoleKeyInfo key, string[] text) 
-        {
-            switch (key.Key)
-            {
-                case ConsoleKey.LeftArrow:
-                    if (x > 0)
-                        x--;
-                    break;
-                case ConsoleKey.RightArrow:
-                    if (x < text[y].Length)
-                        x++;
-                    break;
-                case ConsoleKey.UpArrow:
-                    if (y > 0)
-                        y--;
-                    break;
-                case ConsoleKey.DownArrow:
-                    if (y < text.Length - 1)
-                        y++;
-                    break;
-            }
-        }
         public static void Main(string[] args)
         {
-            Figure figure = new Figure();
-
-            string name = "Квадрат";
-            int width = 10;
-            int height = 10;
-
-            figure.Name = name;
-            figure.Height = height;
-            figure.Width = width;
-            
-
-            while (true)
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Введите путь до файла (вместе с названием), который вы хотите открыть");
+            Console.WriteLine("---------------------------------------------------------------------");
+            Console.SetCursorPosition(0, 3);
+            Console.WriteLine("---------------------------------------------------------------------");
+            Console.ForegroundColor= ConsoleColor.White;
+            Console.Write("Для выхода нажмите ");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("ESCAPE");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write("Для сохранения нажмите ");
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.Write("F1\n");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write("Для изменения текста нажмите ");
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("F2");
+            Console.ResetColor();
+            Console.SetCursorPosition(0, 2);
+            string path = Console.ReadLine();
+            if (Directory.Exists(path))
             {
-                Console.ForegroundColor = ConsoleColor.Yellow;
-                Console.WriteLine("Введите путь до файла (вместе с названием), который вы хотите открыть");
-                Console.WriteLine("---------------------------------------------------------------------");
-                Console.SetCursorPosition(0, 3);
-                Console.WriteLine("---------------------------------------------------------------------");
-                Console.ForegroundColor= ConsoleColor.White;
-                Console.Write("Для выхода нажмите ");
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine("ESCAPE");
-                Console.ForegroundColor = ConsoleColor.White;
-                Console.Write("Для сохранения нажмите ");
-                Console.ForegroundColor = ConsoleColor.Cyan;
-                Console.Write("F1");
-                Console.ResetColor();
                 Console.SetCursorPosition(0, 2);
-                string path = Console.ReadLine();
-                string extension = Path.GetExtension(path).ToLower();
-                switch (extension)
+                int len = path.Length;
+                for (int i = 0; i < len; i++) Console.Write(" ");
+                Console.SetCursorPosition(0, 2);
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Путь не существует!");
+                Console.SetCursorPosition(0, 12);
+                Console.ResetColor();
+                Environment.Exit(1);
+            }
+            TextEditor editor = new TextEditor(path);
+            ConsoleKeyInfo key;
+            do
+            {
+                editor.Display();
+                key = Console.ReadKey();
+
+                switch (key.Key)
                 {
-                    case ".txt":
-                        TextEditor editor = new TextEditor(path);
-                        ConsoleKeyInfo key;
-                        do
-                        {
-                            editor.Display(extension);
-                            key = Console.ReadKey();
-
-                            switch (key.Key)
-                            {
-                                case ConsoleKey.Escape:
-                                    Console.Clear();
-                                    break;
-                                case ConsoleKey.LeftArrow:
-                                case ConsoleKey.RightArrow:
-                                case ConsoleKey.UpArrow:
-                                case ConsoleKey.DownArrow:
-                                    editor.MoveCursor(key);
-                                    break;
-                                case ConsoleKey.F1:
-                                    editor.SaveText(figure);
-                                    break;
-                                case ConsoleKey.Enter:
-                                    break;
-                                default:
-                                    editor.EditText(key);
-                                    break;
-                            }
-
-                            editor.MoveCursor(key);
-
-                        } while (key.Key != ConsoleKey.Escape);
+                    case ConsoleKey.Escape:
+                        Console.Clear();
                         break;
-                    case ".json":
-
+                    case ConsoleKey.F2:
+                        editor.Edit();
                         break;
-                    case ".xml":
+                    case ConsoleKey.F1:
+                        editor.Save();
                         break;
                 }
-            }
+            } while (key.Key != ConsoleKey.Escape);
         }
     }
 }
--- sixth_c_sharp_practo/Text_editor.cs
+++ sixth_c_sharp_practo/Text_editor.cs
@@ -15,146 +15,106 @@ namespace sixth_c_sharp_practo
         private int x;
         private int y;
         private string path;
+        string extension;
+        Figure figure = new Figure();
 
         public TextEditor(string path)
         {
-            text = File.ReadAllLines(path);
             x = 0;
             y = 1;
             this.path = path;
-        }
-        public void Display(string extension)
-        {
+            this.extension = Path.GetExtension(path).ToLower();
+
             switch (extension)
             {
                 case ".txt":
-                    Console.Clear();
-                    Console.ForegroundColor = ConsoleColor.Yellow;
-                    Console.WriteLine("---------------------------------------------------------------------");
-                    Console.SetCursorPosition(0, text.Length+1);
-                    Console.WriteLine("---------------------------------------------------------------------");
-                    Console.ForegroundColor = ConsoleColor.White;
-                    Console.Write("Для выхода нажмите ");
-                    Console.ForegroundColor = ConsoleColor.Red;
-                    Console.WriteLine("ESCAPE");
-                    Console.ForegroundColor = ConsoleColor.White;
-                    Console.Write("Для сохранения нажмите ");
-                    Console.ForegroundColor = ConsoleColor.Cyan;
-                    Console.Write("F1");
-                    Console.ResetColor();
-                    Console.SetCursorPosition(0, 1);
-                    foreach (string line in text)
+                    this.text = File.ReadAllLines(path);
+                    if (text.Length > 3)
                     {
-                        Console.WriteLine(line);
+                        Console.SetCursorPosition(0, 2);
+                        int len = path.Length;
+                        for (int i = 0; i < len; i++) Console.Write(" ");
+                        Console.SetCursorPosition(0, 2);
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Текстовый файл не соответсвует формату фигуры");
+                        Console.SetCursorPosition(0, 12);
+                        Console.ResetColor();
+                        Environment.Exit(1);
+                        Environment.Exit(0);
                     }
-
-                    Console.SetCursorPosition(x, y+1);
+                    else
+                    {
+                        this.figure.Name = this.text[0];
+                        this.figure.Width = Convert.ToInt32(this.text[1]);
+                        this.figure.Height = Convert.ToInt32(this.text[2]);
+                    }
                     break;
                 case ".json":
-
+                    string json = File.ReadAllText(path);
+                    this.figure = JsonConvert.DeserializeObject<Figure>(json);
                     break;
                 case ".xml":
+                    XmlSerializer serializer = new XmlSerializer(typeof(Figure));
 
+                    using (FileStream fileStream = new FileStream(path, FileMode.Open))
+                    {
+                        this.figure = (Figure)serializer.Deserialize(fileStream);
+                    }
                     break;
             }
         }
-
-        public void MoveCursor(ConsoleKeyInfo key)
+        public void Display()
         {
-            switch (key.Key)
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("---------------------------------------------------------------------");
+            Console.WriteLine($"Имя фигуры: {this.figure.Name}");
+            Console.WriteLine($"Ширина фигуры: {this.figure.Width}");
+            Console.WriteLine($"Высота фигуры: {this.figure.Height}");
+            Console.WriteLine("---------------------------------------------------------------------");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write("Для выхода нажмите ");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("ESCAPE");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write("Для сохранения нажмите ");
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.Write("F1\n");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write("Для изменения текста нажмите ");
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.Write("F2");
+            Console.ResetColor();
+        }
+
+
+        public void Edit()
+        {
+            Console.Clear();
+            Console.WriteLine("Выберети что вы хотите изменить");
+            Console.WriteLine("1. Имя");
+            Console.WriteLine("2. Ширина");
+            Console.WriteLine("3. Высота");
+            int choice = Convert.ToInt32(Console.ReadLine());
+            switch (choice)
             {
-                case ConsoleKey.LeftArrow:
-                    if (x > 0)
-                        x--;
-                    else if (y > 0)
-                    {
-                        x = text[y - 1].Length+1;
-                        y--;
-                    }
+                case 1:   Console.Clear();
+                    Console.WriteLine("Введите новое имя: ");
+                    this.figure.Name = Console.ReadLine();
                     break;
-                case ConsoleKey.RightArrow:
-                    if (x < text[y].Length)
-                        x++;
-                    else if (y < text.Length - 1)
-                    {
-                        x = 0;
-                        y++;
-                    }
-                    break;
-                case ConsoleKey.UpArrow:
-                    if (y > 0)
-                    {
-                        if (x > text[y - 1].Length)
-                            x = text[y - 1].Length;
-                        y--;
-                    }
-                    break;
-                case ConsoleKey.DownArrow:
-                    if (y < text.Length - 1)
-                    {
-                        if (x > text[y + 1].Length)
-                            x = text[y + 1].Length;
-                        y++;
-                    }
-                    break;
-                case ConsoleKey.Escape:
+                case 2:
                     Console.Clear();
-                    Environment.Exit(0);
+                    Console.WriteLine("Введите новую ширину: ");
+                    this.figure.Width = Convert.ToInt32(Console.ReadLine());
+                    break;
+                case 3:
+                    Console.Clear();
+                    Console.WriteLine("Введите новую высоту: ");
+                    this.figure.Height = Convert.ToInt32(Console.ReadLine());
                     break;
             }
         }
-
-
-
-
-
-
-        public void EditText(ConsoleKeyInfo keyInfo)
-        {
-            if (char.IsLetterOrDigit(keyInfo.KeyChar) || char.IsPunctuation(keyInfo.KeyChar) ||
-                char.IsWhiteSpace(keyInfo.KeyChar))
-            {
-                text[y] = text[y].Insert(x, keyInfo.KeyChar.ToString());
-                x++;
-            }
-            else if (keyInfo.Key == ConsoleKey.Backspace && x > 0)
-            {
-                text[y] = text[y].Remove(x - 1, 1);
-                x--;
-            }
-        }
-        public void EditJson(ConsoleKeyInfo keyInfo)
-        {
-
-            if (char.IsLetterOrDigit(keyInfo.KeyChar) || char.IsPunctuation(keyInfo.KeyChar) ||
-                char.IsWhiteSpace(keyInfo.KeyChar))
-            {
-                text[y] = text[y].Insert(x, keyInfo.KeyChar.ToString());
-                x++;
-            }
-            else if (keyInfo.Key == ConsoleKey.Backspace && x > 0)
-            {
-                text[y] = text[y].Remove(x - 1, 1);
-                x--;
-            }
-        }
-        public void EditXml(ConsoleKeyInfo keyInfo)
-        {
-
-            if (char.IsLetterOrDigit(keyInfo.KeyChar) || char.IsPunctuation(keyInfo.KeyChar) ||
-                char.IsWhiteSpace(keyInfo.KeyChar))
-            {
-                text[y] = text[y].Insert(x, keyInfo.KeyChar.ToString());
-                x++;
-            }
-            else if (keyInfo.Key == ConsoleKey.Backspace && x > 0)
-            {
-                text[y] = text[y].Remove(x - 1, 1);
-                x--;
-            }
-        }
-
-        public void SaveText(Figure figure)
+        public void Save()
         {
             Console.Clear();
             Console.ForegroundColor = ConsoleColor.Yellow;
@@ -164,34 +124,31 @@ namespace sixth_c_sharp_practo
             Console.WriteLine("-------------------------------------------------");
             Console.SetCursorPosition(0, 2);
             Console.ResetColor();
-            string new_path = Console.ReadLine();
-            File.WriteAllText(new_path, string.Empty);
-            string extension = Path.GetExtension(new_path).ToLower();
-
-            figure.Name = text[0];
-            figure.Width = Convert.ToInt32(text[1]);
-            figure.Height = Convert.ToInt32(text[2]);
-
+            string save_path = Console.ReadLine();
+            string extension = Path.GetExtension(save_path).ToLower();
 
             switch (extension)
             {
                 case ".txt":
-                    using (StreamWriter outputFile = new StreamWriter(new_path))
+                    text[0] = figure.Name;
+                    text[1] = figure.Width.ToString();
+                    text[2] = figure.Height.ToString();
+                    using (StreamWriter outputFile = new StreamWriter(save_path))
                     {
                         foreach (string line in text)
                             outputFile.WriteLine(line);
                     }
                     break;
                 case ".json":
-                    using (FileStream fs = new FileStream(path, FileMode.OpenOrCreate))
+                    using (FileStream fs = new FileStream(save_path, FileMode.OpenOrCreate))
                     {
                         string json = JsonConvert.SerializeObject(figure);
-                        File.WriteAllText(new_path, json);
+                        File.WriteAllText(save_path, json);
                     }
                     break;
                 case ".xml":
                     XmlSerializer serializer = new XmlSerializer(typeof(Figure));
-                    using (FileStream fs = new FileStream(new_path, FileMode.OpenOrCreate))
+                    using (FileStream fs = new FileStream(save_path, FileMode.OpenOrCreate))
                     {
                         serializer.Serialize(fs, figure);
                     }
